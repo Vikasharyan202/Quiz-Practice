3d456b809565cc3fc2800fe56201d13225a4ce30
202vikash@gmail.com <vikash_aryan@202vikashgmailcoms-MacBook-Air.local>
2023-05-06 22:18:07 +0530

    add javascript

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const quizData = [
         b : 'human to computer',
         c : 'human to animal',
         d : 'none of above',
-        correct : b
+        correct : 'b'
     },
     {
         question : 'Most popular progamming language is?',
@@ -13,7 +13,7 @@ const quizData = [
         b : 'python',
         c : 'javascript',
         d : 'c++',
-        correct : c
+        correct : 'c'
     },
     {
         question : 'Which editer you are using for coding?',
@@ -21,7 +21,7 @@ const quizData = [
         b : 'text editer',
         c : 'eclipse',
         d : 'visual studio code',
-        correct : d
+        correct : 'd'
     },
     {
         question : 'what is your favorite topic in programming life?',
@@ -29,7 +29,7 @@ const quizData = [
         b : 'app development',
         c : 'designer',
         d : 'web development',
-        correct : a
+        correct : 'a'
     },
     {
         question : 'what is your qualification?',
@@ -37,6 +37,72 @@ const quizData = [
         b : 'graduate',
         c : 'masters',
         d : 'ph.d',
-        correct : b
+        correct : 'b'
     }
 ];
+
+const questionElem = document.querySelector('#question');
+
+const answerElem = document.querySelectorAll('.answer');
+const quiz = document.querySelector('#quiz');
+
+const a_text = document.querySelector('#a_text');
+const b_text = document.querySelector('#b_text');
+const c_text = document.querySelector('#c_text');
+const d_text = document.querySelector('#d_text');
+const submitBtn = document.querySelector('#submitBtn');
+
+let currentQuiz = 0;
+let score = 0; 
+
+loadQuiz();
+
+function loadQuiz(){
+
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElem.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+
+    let answer = undefined;
+
+    answerElem.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers(){
+    answerElem.forEach((answerEl) => { 
+            answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', ()=> {
+    
+    const answer = getSelected();
+    
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        }else{
+            quiz.innerHTML = `<h2>You answered correctly ${score} / ${quizData.length} questions. </h2>
+            <button onclick="location.reload()"> Reload </button>`;
+        }
+    }
+});
