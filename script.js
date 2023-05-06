d210c03105a4e6f7d97d896266469bd5428fe4d7
202vikash@gmail.com <vikash_aryan@202vikashgmailcoms-MacBook-Air.local>
2023-05-06 14:08:21 +0530

    add strucutre and style

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question : 'Javascript is a which type of language?',
+        a : 'human to human',
+        b : 'human to computer',
+        c : 'human to animal',
+        d : 'none of above',
+        correct : b
+    },
+    {
+        question : 'Most popular progamming language is?',
+        a : 'java',
+        b : 'python',
+        c : 'javascript',
+        d : 'c++',
+        correct : c
+    },
+    {
+        question : 'Which editer you are using for coding?',
+        a : 'online editer',
+        b : 'text editer',
+        c : 'eclipse',
+        d : 'visual studio code',
+        correct : d
+    },
+    {
+        question : 'what is your favorite topic in programming life?',
+        a : 'problem solving',
+        b : 'app development',
+        c : 'designer',
+        d : 'web development',
+        correct : a
+    },
+    {
+        question : 'what is your qualification?',
+        a : 'under graduate',
+        b : 'graduate',
+        c : 'masters',
+        d : 'ph.d',
+        correct : b
+    }
+];
